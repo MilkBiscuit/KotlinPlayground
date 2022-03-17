919c9c36feafec571cbfe529fdcd859a37e8cc1d
Chandler Cheng <dc9909@hotmail.com>
2022-03-17 14:48:27 +1300

    Add more experiment about flow

--- src/main/kotlin/SharedFlowSample.kt
+++ src/main/kotlin/SharedFlowSample.kt
@@ -1,27 +1,39 @@
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 
 suspend fun main() {
-    val flow: MutableSharedFlow<Int> = MutableSharedFlow(replay = 3)
-
-    coroutineScope {
-        flow.emit(1)
-        flow.emit(2)
-        flow.emit(3)
-        flow.emit(4)
-        flow.emit(5)
-    }
+    val mutableFlow = MutableSharedFlow<Int>(replay = 0)
+    val flow: SharedFlow<Int> = mutableFlow.asSharedFlow()
 
     coroutineScope {
         launch {
-            delay(1000)
-            flow.collect {
+            println("starting to emit")
+            delay(2000)
+            mutableFlow.emit(1)
+            mutableFlow.emit(2)
+            mutableFlow.emit(3)
+            mutableFlow.emit(4)
+            mutableFlow.emit(5)
+            mutableFlow.emit(6)
+        }
+
+        launch {
+            println("first value 1, received num: ${flow.firstOrNull()}")
+            println("first value 2, received num: ${flow.firstOrNull()}")
+            println("first value 3, received num: ${flow.firstOrNull()}")
+            println("place 1")
+        }
+
+        launch {
+            delay(300)
+            mutableFlow.collect {
                 println("received num: $it")
             }
+            println("place 2")
         }
-        println("Hello")
+        println("hello main")
     }
 }
