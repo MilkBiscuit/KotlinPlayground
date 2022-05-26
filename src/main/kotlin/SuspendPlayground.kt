8e477421609708213d4471d7cf622c0144b54e83
Chandler Cheng <dc9909@hotmail.com>
2022-05-26 14:03:38 +1200

    Explicit concurrency
    https://elizarov.medium.com/explicit-concurrency-67a8e8fd9b25

--- src/main/kotlin/SuspendPlayground.kt
+++ src/main/kotlin/SuspendPlayground.kt
@@ -3,17 +3,29 @@ import kotlinx.coroutines.*
 
 private suspend fun doSomethingSlow() {
     coroutineScope {
-        doThatIn(this)
+        val infiniteJob = somethingInfinite(this)
+        println("jobs have been launched.")
+        anotherThing()
+        println("The second job done, I want to finish.")
 
-        delay(2000L)
-        println("some job is finished.")
+        infiniteJob.cancel()
+//        cancel()
+        println("Do I get called?")
     }
     println("When do I get called?")
 }
 
-private fun doThatIn(scope: CoroutineScope) {
-    scope.launch {
-        println("I'm fine")
+private suspend fun anotherThing() {
+    delay(1000L)
+    println("Another job done.")
+}
+
+private fun somethingInfinite(scope: CoroutineScope): Job {
+    return scope.launch {
+        for (index in 0 until 50) {
+            delay(200L)
+            println("I'm doing something. $index")
+        }
     }
 }
 
